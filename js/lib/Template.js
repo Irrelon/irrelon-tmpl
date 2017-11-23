d1ffa6270a8284896b11b3728565732fa5629cf3
irrelon <rob@irrelon.com>
2017-11-23 17:25:13 +0000

    Some extra functionality to help support data binding when we have it working

--- js/lib/Template.js
+++ js/lib/Template.js
@@ -4,8 +4,28 @@ var Template = function (core, html) {
 	this._renderFunc = core.parse(html);
 };
 
+/**
+ * Renders the template with the passed data.
+ * @param data
+ * @returns {*}
+ */
 Template.prototype.render = function (data) {
 	return this._renderFunc(data);
+};
+
+/**
+ * Data-binds the template on the DOM to the data object passed.
+ * @param selector
+ * @param data
+ */
+Template.prototype.link = function (selector, data) {
+	// Add this template to the linked views of this data object
+	this._core._linkedViews[data] = this._core._linkedViews[data] || [];
+	this._core._linkedViews[data].push(this);
+};
+
+Template.prototype.updateProperty = function (obj, prop, newVal) {
+
 };
 
 module.exports = Template;
